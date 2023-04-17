5fc1122dece9147957ab03238c093b62a6a59098
Allan Oliveira <allanoliveira.dev@gmail.com>
2023-04-17 01:50:44 -0300

    changed get collection to real time subscription

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app'
 import {
     getFirestore,
     collection,
-    getDocs,
+    onSnapshot,
     addDoc,
     deleteDoc,
     doc
@@ -26,18 +26,16 @@ const db = getFirestore()
 // collection ref
 const colRef = collection(db, 'books')
 
-// get collection data
-getDocs(colRef)
-.then((snapshot) => {
+// get real time collection data
+onSnapshot(colRef, (snapshot) => {
     let books = []
     snapshot.docs.forEach((doc) => {
         books.push({ ...doc.data() , id: doc.id})
     })
     console.log(books)
 })
-.catch(err => {
-    console.log(err.message)
-})
+
+
 
 // adding documents
 const addBookForm = document.querySelector('.add')
